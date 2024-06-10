c56821aed39c09edfb4734c46f5e11dc59c93717
Rxix00 <937088290@qq.com>
2024-06-10 17:34:22 +0800

    1.修改api接口请求地址, 2.添加商品系统-三级分类页面，3.数据渲染，删除操作

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,9 +1,109 @@
 <template>
-  <div>test</div>
+  <div>
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      :expand-on-click-node="false"
+      :default-expanded-keys="expandKeys"
+      show-checkbox
+      node-key="catId"
+    >
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button
+            v-if="data.catLevel <= 2"
+            type="text"
+            size="mini"
+            @click="() => append(data)"
+          >
+            添加
+          </el-button>
+          <el-button
+            v-if="data.children.length == 0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)"
+          >
+            删除
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+  </div>
 </template>
 
 <script>
-export default {}
+/* eslint-disable */
+export default {
+  data() {
+    return {
+      data: [],
+      expandKeys: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    }
+  },
+  methods: {
+    getCategory() {
+      this.$http({
+        url: this.$http.adornUrl("/product/category/listTree"),
+        method: "get"
+      }).then(({ data }) => {
+        console.log("成功获取的类别数据：", data.data)
+        this.data = data.data
+      })
+    },
+    append(data) {
+      // this.open(data)
+      console.log("添加", data)
+    },
+    remove(node, data) {
+      this.open(node, data)
+      console.log("删除", data, node)
+    },
+    open(node, data) {
+      this.$confirm(`是否确认删除【${data.name}】记录?`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          let ids = [data.catId]
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false)
+          }).then(({ data }) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: "删除成功",
+                type: "success"
+              })
+
+              //刷新页面
+              this.getCategory()
+              //保持展示的页面还是当前删除的父节点
+              this.expandKeys = [node.parent.data.catId]
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          })
+        })
+    }
+  },
+  created() {
+    this.getCategory()
+  }
+}
 </script>
 
 <style></style>
